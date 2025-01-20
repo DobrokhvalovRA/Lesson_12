5a4b86cecc0c95872e13ec43f653bf0b5098e4f1
DobrokhvalovRA <DobrokhvalovRA@icloud.com>
2025-01-20 08:48:18 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,28 @@
 public class Main {
 
-    public static void checkLengthArray(String[][] array) throws MyArrayExeption {
-        if (array.length > 4 || array[0].length > 4) {
-            throw new MyArrayExeption("Ой, он слишком большой!");
+    public static void checkArray(String[][] array) throws MyArraySizeExeption,MyArrayDataExeption {
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeExeption("Ой, не тот размер!");
         }
-    }
 
-    public static void checkCharArray(String[][] array) throws MyArrayExeption {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[0].length; j++) {
                 if (!array[i][j].matches(("-?\\d+"))) {
-                    throw new MyArrayExeption("Ой, тут в ячейке: " + i + "-" + j + " не цыфра, а вон че - " + array[i][j]);
+                    throw new MyArrayDataExeption("Ой, тут в ячейке: " + i + "-" + j + " не цыфра, а вон че - " + array[i][j]);
                 }
             }
         }
     }
+
+    /*/public static void checkCharArray(String[][] array) throws MyArrayDataExeption {
+       for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                if (!array[i][j].matches(("-?\\d+"))) {
+                    throw new MyArrayDataExeption("Ой, тут в ячейке: " + i + "-" + j + " не цыфра, а вон че - " + array[i][j]);
+                }
+            }
+        }
+    }/*/
 
     public static void fillArray(String[][] array) {
         for (int i = 0; i < array.length; i++) {
@@ -37,15 +45,18 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        String[][] array = new String[3][4];
+        String[][] array = new String[4][4];
         fillArray(array);
-        array[1][3] = "3";
+        array[1][1] = "3";
 
         try {
-            checkCharArray(array);
-            checkLengthArray(array);
+            //checkCharArray(array);
+            //checkLengthArray(array);
+            checkArray(array);
             System.out.println(sumArray(array));
-        } catch (MyArrayExeption e) {
+        } catch (MyArraySizeExeption e) {
+            System.out.println("Ошибочка: " + e.getMessage());
+        } catch (MyArrayDataExeption e) {
             System.out.println("Ошибочка: " + e.getMessage());
         }
     }
